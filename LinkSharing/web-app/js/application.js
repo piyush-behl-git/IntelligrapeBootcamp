a1ff3ae2fc5798f61d975aa9879ca04aca321181
Puneet <puneet.behl@intelligrape.com>
2012-08-31 19:21:34 +0530

    check/uncheck/inverse func defined in application.js

--- LinkSharing/web-app/js/application.js
+++ LinkSharing/web-app/js/application.js
@@ -1,5 +1,7 @@
-var urls ={
-    checkEmailUrl:""
+var urls = {
+    checkEmailUrl:"",
+    checkDocUrl:"",
+    checkDocContentTypeUrl:""
 };
 if (typeof jQuery !== 'undefined') {
     (function ($) {
@@ -31,72 +33,92 @@ $(document).ready(function () {
         }
     });
     $('#resource-dialog').dialog({
-        autoOpen: false,
-        draggable: false,
-        modal: true,
-        show: 'fadeIn',
-        hide: 'fadeOut',
+        autoOpen:false,
+        draggable:false,
+        modal:true,
+        show:'fadeIn',
+        hide:'fadeOut',
         width:700,
         resizable:false,
         buttons:{
-            Ok:function(){
+            Ok:function () {
                 $(this).dialog("close")
             },
-            Cancel: function() {
+            Cancel:function () {
                 $(this).dialog("close")
             }
         }
 
     });
-    $('#addResource').bind("click",function(){
+    $('#addResource').bind("click", function () {
         $('#resource-dialog').dialog("open")
     });
-    $('#document-dialog').dialog({
-        autoOpen: false,
-        draggable: false,
-        modal: true,
-        show: 'fadeIn',
-        hide: 'fadeOut',
-        width:800,
-        resizable:false,
-        buttons:{
-            Ok:function(){
-                $('form[name=docForm]').submit()
-                $(this).dialog("close")
-            },
-            Cancel: function() {
-                $(this).dialog("close")
-            }
-        }
-
-    });
-    $('#docButton').bind("click",function(){
+    $('#docButton').bind("click", function () {
         $('#resource-dialog').dialog("close")
         $('#document-dialog').dialog('open')
     });
-    $('#link-dialog').dialog({
-        autoOpen: false,
-        draggable: false,
-        modal: true,
-        show: 'fadeIn',
-        hide: 'fadeOut',
+    $('#document-dialog').dialog({
+        autoOpen:false,
+        draggable:false,
+        modal:true,
+        show:'fadeIn',
+        hide:'fadeOut',
         width:800,
         resizable:false,
         buttons:{
-            Ok:function(){
-                $('form[name=linkForm]').submit()
+            Ok:function () {
+                $('form[name=docForm]').submit()
                 $(this).dialog("close")
             },
-            Cancel: function() {
+            Cancel:function () {
                 $(this).dialog("close")
             }
         }
 
     });
-    $('#linkButton').bind("click",function(){
+    $('#link-dialog').dialog({
+        autoOpen:false,
+        draggable:false,
+        modal:true,
+        show:'fadeIn',
+        hide:'fadeOut',
+        width:800,
+        resizable:false,
+        buttons:{
+            Ok:function () {
+                $('form[name=linkForm]').submit()
+                $(this).dialog("close")
+            },
+            Cancel:function () {
+                $(this).dialog("close")
+            }
+        }
+
+    });
+    $('#linkButton').bind("click", function () {
         $('#resource-dialog').dialog("close")
         $('#link-dialog').dialog('open')
     });
 
+    $('select#selectTopic').bind('change', function () {
+        if ($(this).val() == 'Check All') {
+            $('input[name=status]').each(function () {
+                $(this).attr('checked', true)
+            });
+        }
+        if ($(this).val() == 'Uncheck All') {
+            $('input[name=status]').each(function () {
+                $(this).attr('checked', false)
+            });
+        }
+        if ($(this).val() == 'Inverse') {
+            $('input[name=status]').each(function () {
+                if ($(this).is(':checked'))
+                    $(this).attr('checked', false)
+                else
+                    $(this).attr('checked', true)
+            });
+        }
+    });
 });
 
