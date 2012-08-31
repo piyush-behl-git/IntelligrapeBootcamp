42fe34f8c3b341ee3e1b994727ef2d86513d48b3
Puneet <puneet.behl@intelligrape.com>
2012-08-31 20:32:20 +0530

    refactored

--- LinkSharing/grails-app/services/com/ig/bc/DocumentResourceService.groovy
+++ LinkSharing/grails-app/services/com/ig/bc/DocumentResourceService.groovy
@@ -2,14 +2,4 @@ package com.ig.bc
 
 class DocumentResourceService {
 
-    def resourceService
-
-    //TODO move to User domain
-    def getCurrentUserDocumentResourceList(String currentUserEmail) {
-        List<Resource> currentUserResourceList = resourceService.getCurrentUserResources(currentUserEmail)
-        List<DocumentResource> currentUserDocumentResourceList = currentUserResourceList.collect { resource->
-                    if(resource.instanceOf(DocumentResource))
-                        return resource
-        }
-    }
 }
