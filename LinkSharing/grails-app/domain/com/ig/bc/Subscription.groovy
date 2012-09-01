93415d3ca3815f910816527f5abb0ad39645b71f
Puneet <puneet.behl@intelligrape.com>
2012-09-01 13:44:18 +0530

    now if user or topic removed subscription will also be removed

--- LinkSharing/grails-app/domain/com/ig/bc/Subscription.groovy
+++ LinkSharing/grails-app/domain/com/ig/bc/Subscription.groovy
@@ -1,14 +1,32 @@
 package com.ig.bc
 
 import com.ig.bc.enums.Seriousness
+import com.ig.bc.dto.TopicSubscriptionDTO
+import com.ig.bc.enums.Visibility
 
 class Subscription {
-    User subscriber
-    Topic topic
     Date dateCreated
     Date lastUpdated
     Seriousness seriousness
+
+    static belongsTo = [subscriber: User, topic: Topic]
+    static final MAX_RESULT_COUNT = 10
     static constraints = {
+    }
+
+    List<TopicSubscriptionDTO> getTopicAndSubscriptionCount() {
+        List<TopicSubscriptionDTO> topicSubscriptionDTOs = Subscription.createCriteria().list {
+            projections {
+                groupProperty('topic')
+                count('topc', 't')
+            }
+            'topic' {
+                eq("visibility", Visibility.PUBLIC)
+            }
+            maxResults MAX_RESULT_COUNT
+            order('t', 'desc')
+        }
+        return topicSubscriptionDTOs
     }
 
     String toString() {
