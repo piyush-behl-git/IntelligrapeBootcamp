8d83e3f4397d5b4f8dc352cec53f4b8c5e7861f8
Puneet <puneet.behl@intelligrape.com>
2012-09-01 13:52:09 +0530

    user service refactored

--- LinkSharing/grails-app/services/com/ig/bc/UserService.groovy
+++ LinkSharing/grails-app/services/com/ig/bc/UserService.groovy
@@ -2,20 +2,4 @@ package com.ig.bc
 
 class UserService {
 
-    def validateUser(String email, String password) {
-        boolean userStatus
-        User user = User.findByEmailAndPassword(email, password)
-        if (user)
-            userStatus = true
-        else
-            userStatus = false
-        return userStatus
-    }
-
-    //TODO move to user domain
-    List<String> getAllRegisteredEmails() {
-        List<User> userList = User.list()
-        List<String> registeredEmails = userList*.email
-        return registeredEmails
-    }
 }
